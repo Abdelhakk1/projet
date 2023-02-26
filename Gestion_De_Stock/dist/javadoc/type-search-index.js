60b6f5118c99450e474b0a67293c5340ad11c572
Mustapha <bouchenemustapha@gmail.com>
2023-02-26 11:58:56 +0100

    added java doc and updated data

--- Gestion_De_Stock/dist/javadoc/type-search-index.js
+++ Gestion_De_Stock/dist/javadoc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Systeme","l":"Auteur"},{"p":"Systeme","l":"Login_Form_Gestion"},{"p":"Systeme","l":"Register"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Systeme","l":"Login_Form_Gestion"},{"p":"Systeme","l":"Produits"},{"p":"Systeme","l":"Register"}];updateSearchResults();
